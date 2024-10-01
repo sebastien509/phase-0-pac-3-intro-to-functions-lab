e01ff5f9dbe9f56f6559a4378151c24c5c67ec55
Sebastien Fenelon <fenelonsebastien@outlook.com>
2024-10-01 11:31:06 -0500

    Intro to functions lab completed under the index.js file

---
+++ index.js
@@ -0,0 +1,33 @@
+function shout(string){
+    return string.toUpperCase();
+}
+function whisper(string){
+    return string.toLowerCase();
+}
+console.log(logshout(string))
+
+
+function logShout(string) {
+    console.log(string.toUpperCase());
+  }
+  
+  function logWhisper(string) {
+    console.log(string.toLowerCase());
+  }
+  
+  function sayHiToHeadphonedRoommate(string) {
+    if(string === "Let's have dinner together!"){
+        return "I would love to!"
+    }
+    if (string === string.toLowerCase()) {
+      return "I can't hear you!";
+    } else {
+      return "YES INDEED!"; 
+    }
+}
+
+
+  
+    
+  
+  
